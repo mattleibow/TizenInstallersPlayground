3853c9f241552a3154d3a064d213e09a3bc86cb1
Matthew Leibowitz <mattleibow@live.com>
2022-04-16 02:04:24 +0800

    improve

--- create.ps1
+++ create.ps1
@@ -1,2 +1,15 @@
-.\create-installer.ps1
-.\create-nuget.ps1
+foreach ($dir in (gci nugets)) {
+    try {
+        Push-Location $dir/data
+        & "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" (gci *.wxs)
+        & "C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" (gci *.wixobj)
+    } finally {
+        Pop-Location
+    }
+    try {
+        Push-Location $dir
+        & "C:\Projects\nuget.exe" pack -OutputDirectory ..\..\feed
+    } finally {
+        Pop-Location
+    }
+}
